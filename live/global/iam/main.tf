7ba1987cef50070cefc8de86cc3bb3323eae3146
armzig <bellotaophyc@outlook.com>
2023-10-20 16:24:30 +0100

    starting out with defining the iamuser

---
+++ live/global/iam/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_iam_user" "user" {
+
+  name = var.user_name
+
+}
